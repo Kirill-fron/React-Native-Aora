7353a5c6301bb410175b4c360c745bf9b8255cda
Kirill <shakirov.kirill@internet.ru>
2024-12-11 15:54:21 +0500

    development 2 components (custom Buttom, Form), sign-in, sign-up

--- app/(auth)/_layout.jsx
+++ app/(auth)/_layout.jsx
@@ -1,10 +1,16 @@
-import { Text } from "react-native";
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 
 const AuthLayout = () => {
   return (
-    <Stack>
-      <Text>hello</Text>
-    </Stack>
+    <>
+      <Stack>
+        <Stack.Screen name="sign-in" options={{ headerShown: false }} />
+        <Stack.Screen name="sign-up" options={{ headerShown: false }} />
+      </Stack>
+
+      <StatusBar backgroundColor="#161622" style="light" />
+    </>
   );
 };
 
